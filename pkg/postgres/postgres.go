6e802274439f9d731f981f2c25f451d738b21a8c
Лёва Нерсесян <lyova123454321@gmail.com>
2025-03-13 20:51:29 +0300

    pgxpool

--- pkg/postgres/postgres.go
+++ pkg/postgres/postgres.go
@@ -5,20 +5,34 @@ import (
 	"fmt"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type Config struct {
 	Host     string `yaml:"POSTGRES_HOST" env:"POSTGRES_HOST" env-default:"localhost"`
-	Port     string `yaml:"POSTGRES_PORT" env:"POSTGRES_PORT" env-default:"5432"`
+	Port     uint16 `yaml:"POSTGRES_PORT" env:"POSTGRES_PORT" env-default:"5432"`
 	Username string `yaml:"POSTGRES_USER" env:"POSTGRES_USER" env-default:"root"`
 	Password string `yaml:"POSTGRES_PASS" env:"POSTGRES_PASS" env-default:"1234"`
 	Database string `yaml:"POSTGRES_DB" env:"POSTGRES_DB" env-default:"postgres"`
+	MaxConns int32  `yaml:"POSTGRES_MAX_CONN" env:"POSTGRES_MAX_CONN" env-default:"10"`
+	MinConns int32  `yaml:"POSTGRES_MIN_CONN" env:"POSTGRES_MIN_CONN" env-default:"5"`
 }
 
-func New(config *Config) (*pgx.Conn, error) {
-	connString := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
-		config.Username, config.Password, config.Host, config.Port, config.Database)
-	conn, err := pgx.Connect(context.Background(), connString)
+func New(ctx context.Context, config *Config) (*pgxpool.Pool, error) {
+	conn, err := pgxpool.NewWithConfig(ctx, &pgxpool.Config{
+		ConnConfig: &pgx.ConnConfig{
+			Config: pgconn.Config{
+				Host:     config.Host,
+				Port:     config.Port,
+				User:     config.Username,
+				Password: config.Password,
+				Database: config.Database,
+			},
+		},
+		MaxConns: config.MaxConns,
+		MinConns: config.MinConns,
+	})
 	if err != nil {
 		return nil, fmt.Errorf("pgx.Connect: %w", err)
 	}
